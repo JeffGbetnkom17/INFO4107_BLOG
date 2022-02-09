4cecc5458eddce507f5ee3014cec4f52f7ff9693
Jeff GBETNKOM NJIFON <gbetnkomjeffnjifon@gmail.com>
2022-02-09 13:37:25 +0100

    Contact page realised

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+    include("includes/header.php");
+?>
+                    <div class="blog-header">
+                        <h2>Contact us</h2>
+                    </div>
+                    
+                    <div class="blog-srms-form">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label">Username</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label">Password</label>
+                                <div class="col-sm-8">
+                                    <input type="password" name="password" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div><!-- /.blog-main -->
+
+                <?php include("includes/footer.php"); ?>  
